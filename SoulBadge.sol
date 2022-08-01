aef2e7d398f44fdd7c448f050f2548e6ac780de2
RasyidRed <93521724+rasyidred@users.noreply.github.com>
2022-08-01 16:15:00 +0900

    Update SoulBadge.sol

--- SoulBadge.sol
+++ SoulBadge.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+// import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
@@ -11,7 +12,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
  * @title SoulBagde
- * Non-tranferrable digital badge using Soulbound Token
+ * Gimana cara transfer NFTnya nanti dengan kode referral
  */
 
 contract SoulBadge is ERC721URIStorage, Ownable, ReentrancyGuard, ERC721Burnable {
@@ -19,20 +20,18 @@ contract SoulBadge is ERC721URIStorage, Ownable, ReentrancyGuard, ERC721Burnable
     Counters.Counter private _tokenIdCounter;
 
     address public contractOwner;
-    address public storageAddress;
 
     modifier checkValid(bytes32 secretCode){
         require(eventDetails[secretCode].isExist == true, "The code is not valid nor exist");
         _;
     }
 
-    constructor(address _storageAddress) ERC721("SoulBadge", "SBT"){
+    constructor() ERC721("SoulBadge", "SBT"){
         contractOwner = msg.sender;
-        storageAddress = _storageAddress;
     }
 
     event eventCreated (uint maxAttendees, string eventInfo, bytes32 secretCode);
-    
+    event badgeDistributed (address recipient, uint tokenId, string tokenURI);
 
     struct Claimer{
         address attendee;
@@ -58,7 +57,7 @@ contract SoulBadge is ERC721URIStorage, Ownable, ReentrancyGuard, ERC721Burnable
     // }
     
 
-    function mintNFTs(uint _noOfAttendees, string memory _tokenURI) public onlyOwner nonReentrant returns (bytes32 secretCode){
+    function createEvent(uint _noOfAttendees, string memory _tokenURI) public onlyOwner nonReentrant returns (bytes32 secretCode){
         secretCode = keccak256(abi.encodePacked(block.timestamp, _noOfAttendees, _tokenURI));
         uint startingTokenId;
         if (_tokenIdCounter.current() == 0){
@@ -81,19 +80,19 @@ contract SoulBadge is ERC721URIStorage, Ownable, ReentrancyGuard, ERC721Burnable
             
             _safeMint(contractOwner, tokenId);
             _setTokenURI(tokenId, _tokenURI);
-            setApprovalForAll(storageAddress, true);
+            // setApprovalForAll(storageAddress, true); 
         }
-        
         emit eventCreated(_noOfAttendees, _tokenURI, secretCode);
+
         return secretCode;
     }
 
-    function claim(bytes32 _secretCode, address _to) public checkValid(_secretCode) nonReentrant returns (uint tokenId, uint remainingTokens) {
+    function distributeBadges(bytes32 _secretCode, address _to) public checkValid(_secretCode) nonReentrant onlyOwner returns (uint tokenId) {
         bool isClaimed = eventDetails[_secretCode].attendeeDetails[_to].claimed;
         require(isClaimed == false, "Recipient already claimed the Token");
 
         //require max claim
-        remainingTokens = getRemainingTokens(_secretCode);
+        uint remainingTokens = getRemainingTokens(_secretCode);
         require(remainingTokens > 0, "The maximum claims reached!");
         
         //record all attendees' details who are already claimed to Details struct
@@ -104,11 +103,14 @@ contract SoulBadge is ERC721URIStorage, Ownable, ReentrancyGuard, ERC721Burnable
         //record each attendee that is already claimed, to prevent double spending/minting to the same address
         eventDetails[_secretCode].attendeeDetails[_to] = Claimer(_to, tokenId, true);
 
-        // transfer ownership dari minter (contractOwner) ke attendee => move to BadgeStorage
-        // safeTransferFrom(contractOwner, _to, tokenId);
+        // transfer ownership dari minter (contractOwner) ke attendee
+        safeTransferFrom(contractOwner, _to, tokenId);
 
         eventDetails[_secretCode].counterClaimed += 1;
-        return (tokenId, remainingTokens);
+
+        emit badgeDistributed(_to, tokenId, eventDetails[_secretCode].URI);
+
+        return tokenId;
     }
 
     /* Getter Functions */
@@ -123,7 +125,7 @@ contract SoulBadge is ERC721URIStorage, Ownable, ReentrancyGuard, ERC721Burnable
         tokenId = eventDetails[secretCode].counterClaimed + eventDetails[secretCode].startingTokenId;
     }
 
-    function getClaimer(bytes32 secretCode) public view returns (address[] memory addresses, uint[] memory tokenIds, bool[] memory claimed){
+    function getAttendees(bytes32 secretCode) public view returns (address[] memory addresses, uint[] memory tokenIds, bool[] memory claimed){
         
         addresses = new address[](eventDetails[secretCode].maxAttendees);
         tokenIds  = new uint[](eventDetails[secretCode].maxAttendees);
@@ -148,7 +150,7 @@ contract SoulBadge is ERC721URIStorage, Ownable, ReentrancyGuard, ERC721Burnable
     internal override(ERC721) {
         // gak bisa ditransfer kalau udah dikasih dari address contract ini
         require(from == address(0) || to == address(0) ||
-                from == storageAddress || to == storageAddress, "Err: token is SOUL BOUND");
+                from == contractOwner, "Err: token is SOUL BOUND");
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
@@ -161,64 +163,4 @@ contract SoulBadge is ERC721URIStorage, Ownable, ReentrancyGuard, ERC721Burnable
         return super.tokenURI(tokenId);
     }
 
-}
-
-/* SOULBADGE STORAGE TO MAKE CLAIM EASIER */
-
-contract BadgeStorage is ReentrancyGuard, Ownable{
-    using Counters for Counters.Counter;
-
-    Counters.Counter private itemIds;
-    Counters.Counter private itemsRedeemed;
-
-    struct StorageItem{
-        address nftContract;
-        uint tokenId;
-        address creator; //NFT creator
-        address owner; //owner after it's being transfered
-        bool claim;
-    }
-
-    mapping(uint => StorageItem) private idToStorageItem;
-
-    event ItemStored(address indexed nftContract,
-                     uint indexed tokenId,
-                     address creator,
-                     address owner,
-                     bool claim);
-
-    event badgeClaimed (address recipient, uint tokenId, uint remainingTokens);
-
-    /* Stores an NFT on the storage before distributed */ 
-    // create market item
-    function storeItem(address nftContract, uint tokenId) public payable nonReentrant onlyOwner{
-        require(idToStorageItem[tokenId].claim == false, "You can't store a redeemed token!");
-              
-        idToStorageItem[tokenId] = StorageItem(nftContract, tokenId, payable(msg.sender), payable(address(0)), false); 
-        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
-
-        emit ItemStored(nftContract, tokenId, msg.sender, address(0), false);
-    }                
-
-    /* Transfers the NFT's ownership to attendee (msg.sender)*/
-    // buy nft
-    function claimBadge(address nftContract, bytes32 secretCode) public nonReentrant{
-        SoulBadge _SoulBadge = SoulBadge(nftContract); 
-
-        require(msg.sender != owner(), "Only attendees that can claim!");
-        (uint tokenId, uint remainingTokens) = _SoulBadge.claim(secretCode, msg.sender);
-
-        idToStorageItem[tokenId].owner = (msg.sender);
-        idToStorageItem[tokenId].claim = true;
-
-        IERC721(_SoulBadge).transferFrom(address(this), msg.sender, tokenId);
-
-        emit badgeClaimed (msg.sender, tokenId, remainingTokens);
-    }
-    
-    /*Returns all NFTs which has not been claimed*/
-
-
-    // /*Returns all NFTs which has been redeemed*/
-    
 }
